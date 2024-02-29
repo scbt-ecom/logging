7450531d9afcd241e5e63f87a55c548a98c00293
Trofimov Alexander Alekseevich <trofimovaa2@sovcombank.ru>
2024-02-29 14:39:43 +0300

    fix: method instead function

--- graylog.go
+++ graylog.go
@@ -4,7 +4,7 @@ import (
 	graylog "github.com/gemnasium/logrus-graylog-hook"
 )
 
-func AddGraylogHook(log *Logger, graylogUrl, containerName string) {
+func (log *Logger) AddGraylogHook(graylogUrl, containerName string) {
 	hook := graylog.NewGraylogHook(graylogUrl, map[string]interface{}{"container_name": containerName})
 	log.Logger.AddHook(hook)
 }
